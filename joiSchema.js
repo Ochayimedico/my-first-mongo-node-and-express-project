fe2f227f9f353d5540719e9e2514bc948211acfc
Ochayi Peter <ochayipeter42@gmail.com>
2022-09-14 20:26:40 +0100

    second commit

--- joiSchema.js
+++ joiSchema.js
@@ -2,6 +2,6 @@ const Joi = require("joi");
 
 module.exports.productSchema = Joi.object({
   name: Joi.string().required(),
-  price: Joi.number().required().min(1).max(5),
+  price: Joi.number().required().min(1).max(10),
   category: Joi.required(),
 });
